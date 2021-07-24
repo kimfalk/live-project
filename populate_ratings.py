0441a012d8741af3df0862013cf2008426fd38e3
kim falk <k@briight.io>
2021-07-24 16:50:58 +0200

    Fix: Update packages and fix data import.

--- populate_ratings.py
+++ populate_ratings.py
@@ -5,7 +5,7 @@ import datetime
 import decimal
 from tqdm import tqdm
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prs_project.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prs_project.settings")
 
 django.setup()
 
@@ -14,26 +14,31 @@ from analytics.models import Rating
 
 def create_rating(user_id, content_id, rating, timestamp):
 
-    rating = Rating(user_id=user_id, movie_id=content_id, rating=decimal.Decimal(rating),
-                    rating_timestamp=datetime.datetime.fromtimestamp(float(timestamp)))
+    rating = Rating(
+        user_id=user_id,
+        movie_id=content_id,
+        rating=decimal.Decimal(rating),
+        rating_timestamp=datetime.datetime.fromtimestamp(float(timestamp)),
+    )
     rating.save()
 
     return rating
 
 
 def download_ratings():
-    URL = 'https://raw.githubusercontent.com/sidooms/MovieTweetings/master/latest/ratings.dat'
+    URL = "https://raw.githubusercontent.com/sidooms/MovieTweetings/master/latest/ratings.dat"
     response = urllib.request.urlopen(URL)
     data = response.read()
 
-    print('download finished')
-    return data.decode('utf-8')
+    print("download finished")
+    return data.decode("utf-8")
 
 
 def delete_db():
-    print('truncate db')
+
+    print("truncate db")
     Rating.objects.all().delete()
-    print('finished truncate db')
+    print("finished truncate db")
 
 
 def populate():
@@ -48,6 +53,6 @@ def populate():
             create_rating(r[0], r[1], r[2], r[3])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Starting MovieGeeks Population script...")
     populate()
--- prs_project/settings.py
+++ prs_project/settings.py
@@ -2,15 +2,15 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2jak@usd23kz0&+kxh2%mx24mw2zw5692s-#y0s$30*9z2y2+x'
+SECRET_KEY = "2jak@usd23kz0&+kxh2%mx24mw2zw5692s-#y0s$30*9z2y2+x"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -21,68 +21,70 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_extensions',
-    'moviegeeks',
-    'collector',
-    'analytics',
-    'recommender',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django_extensions",
+    "moviegeeks",
+    "collector",
+    "analytics",
+    "recommender",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'prs_project.urls'
+ROOT_URLCONF = "prs_project.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR, ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            TEMPLATE_DIR,
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'prs_project.wsgi.application'
+WSGI_APPLICATION = "prs_project.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #   'default': {
 #       'ENGINE': 'django.db.backends.sqlite3',
 #       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #   }
-#}
+# }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'moviegeeks',
-        'USER': 'postgres',
-        'PASSWORD': 'pw1234',
-        'HOST': 'db',
-        'PORT': 5432,
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "moviegeeks",
+        "USER": "postgres",
+        "PASSWORD": "pw1234",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 # Password validation
@@ -90,16 +92,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -107,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -121,6 +123,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [STATIC_DIR, ]
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
